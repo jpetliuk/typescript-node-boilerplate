c6c56b6f63e1b56c7b5a42475e97fe3f7b44ba78
Jeremias Petliuk <74077990+jpetliuk@users.noreply.github.com>
2025-03-13 18:43:03 -0300

    Update add.test.ts

--- tests/add.test.ts
+++ tests/add.test.ts
@@ -1,5 +1,11 @@
 import { add } from '../src/example';
 
-it('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
+describe('add function', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+  });
+
+  it('adds 5 + 2 to equal 7', () => {
+    expect(add(5, 2)).toBe(7);
+  });
 });
