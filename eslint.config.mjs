f17da0374987d46b00b2ab1f7d0d6813d9bfd79c
jpetliuk <jeremias.petliuk@gmail.com>
2025-03-13 02:24:13 -0300

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import jest from "eslint-plugin-jest";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ["dist/"] },
+  { files: ["src/**/*.{js,ts,jtx,tsx}", "tests/**/*.{js,ts,jtx,tsx}"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["tests/**/*.{js,ts,jtx,tsx}"],
+    ...jest.configs["flat/recommended"],
+    rules: {
+      ...jest.configs["flat/recommended"].rules,
+      "jest/prefer-expect-assertions": "off",
+    },
+  },
+  eslintPluginPrettierRecommended,
+];
